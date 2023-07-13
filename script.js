5996890564301055591507191f1b213bed3f8974
SURIYA <53847084+ssuriya1@users.noreply.github.com>
2023-07-14 00:00:39 +0530

    Dynamic code update for the card data.

---
+++ script.js
@@ -0,0 +1,71 @@
+// Function to update the card based on JSON data
+function updateCard(jsonData) {
+  var chatCard = document.getElementById("chatCard");
+  chatCard.innerHTML = "";
+
+  jsonData.forEach(function (data) {
+    let anchorCard = document.createElement("a");
+    anchorCard.href = data.url;
+    anchorCard.classList.add("anchorCard");
+
+    var card = document.createElement("div");
+    card.classList.add("card");
+
+    if (data.title) {
+      var title = document.createElement("div");
+      title.classList.add("title");
+      title.textContent = data.title;
+      card.appendChild(title);
+    }
+
+    if (data.imageURL) {
+      var image = document.createElement("img");
+      image.classList.add("image");
+      image.src = data.imageURL;
+      image.alt = data.imageURL;
+      card.appendChild(image);
+    }
+
+    if (data.description) {
+      var description = document.createElement("div");
+      description.classList.add("description");
+      description.textContent = data.description;
+      card.appendChild(description);
+    }
+
+    anchorCard.appendChild(card);
+    chatCard.appendChild(anchorCard);
+  });
+}
+
+function changeClass(e) {
+  e.classList.toggle("off");
+  e.classList.toggle("on");
+}
+document.querySelectorAll("span").forEach((span) =>
+  span.addEventListener("click", function () {
+    changeClass(this);
+  })
+);
+
+// JSON data stored as a variable
+var jsonData = [
+  {
+    title: "Guess the Number",
+    imageURL: "guess the number.png",
+    url: "Guess The Number/index.html",
+  },
+  {
+    title: "Ping Pong",
+    imageURL: "Canvas.png",
+    url: "Canvas/index.html",
+  },
+  {
+    title: "Quiz",
+    imageURL: "Quiz.png",
+    description: "Unable to play this game in browser.",
+    url: "#",
+  },
+];
+
+updateCard(jsonData);
