349ada1ab0ee92fb66776d8526a1b8ba231f14e7
SURIYA <53847084+ssuriya1@users.noreply.github.com>
2023-07-14 23:31:51 +0530

    additional changes

--- script.js
+++ script.js
@@ -63,25 +63,3 @@ if (modeToggle !== undefined || null) {
     }
   });
 }
-
-// JSON data stored as a variable
-var jsonData = [
-  {
-    title: "Guess the Number",
-    imageURL: "guess the number.png",
-    url: "Guess The Number/index.html",
-  },
-  {
-    title: "Ping Pong",
-    imageURL: "Canvas.png",
-    url: "Canvas/index.html",
-  },
-  {
-    title: "Quiz",
-    imageURL: "Quiz.png",
-    description: "Unable to play this game in browser.",
-    url: "#",
-  },
-];
-
-updateCard(jsonData);
