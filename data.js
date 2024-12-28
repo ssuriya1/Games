d429d470b63fe08878dff6fddd25be835d41fc93
suriya <suriyastar100@gmail.com>
2024-12-28 18:12:07 +0530

    Refactor audio file paths and rename game HTML files for consistency

--- data.js
+++ data.js
@@ -1,30 +1,40 @@
 // JSON data stored as a variable
 var jsonData = [
   {
-    title: "Guess the Number",
-    imageURL: "./assets/guess the number.png",
-    url: "Guess The Number/index.html",
-  },
-  {
     title: "Ping Pong",
-    imageURL: "./assets/Canvas.png",
-    url: "Canvas/index.html",
-  },
-  {
-    title: "Tower-of-hanoi",
-    imageURL: "./assets/TOH.png",
-    url: "Tower-of-hanoi/index.html",
+    imageURL: "./assets/images/Ping Pong.png",
+    url: "Ping-Pong",
   },
   {
     title: "Hangman",
-    imageURL: "./assets/Hangman.png",
-    url: "Hangman/index.html",
+    imageURL: "./assets/images/Hangman.png",
+    url: "Hangman",
+  },
+  {
+    title: "Snake",
+    imageURL: "./assets/images/Snake.png",
+    url: "Snake",
+  },
+  {
+    title: "Shooter",
+    imageURL: "./assets/images/Shooter.png",
+    url: "Shooter",
+  },
+  {
+    title: "Guess the Number",
+    imageURL: "./assets/images/guess the number.png",
+    url: "Guess-The-Number",
+  },
+  {
+    title: "Tower of Hanoi",
+    imageURL: "./assets/images/TOH.png",
+    url: "Tower-Of-Hanoi",
   },
   {
     title: "Quiz",
     imageURL: "",
     description: "Unable to play this game in browser.",
-    url: "#",
+    url: "",
   },
 ];
 
